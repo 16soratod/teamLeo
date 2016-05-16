b180692db825e0c371cc5c4b84051f0de3a4fd3f
Elina Hvirtsman <elina.hvirtsman@gmail.com>
2016-05-16 13:18:40 -0400

    Started constructors

--- Sudoku.java
+++ Sudoku.java
@@ -1,3 +1,31 @@
+import java.util.*;
+import java.io.*;
+
 public class Sudoku{
-	int[][] puzzle =  { { } }
+    private int[][] puzzle;
+
+    public Sudoku(String filename){
+	try {
+	    Scanner in = new Scanner(new File(filename));
+	    puzzle = new int[9][9];
+	    String s = ""
+	    While (in.hasNextLine()){
+		s += in.nextLine();
+	    }
+	    for (int i = 0; i < 9; i++){
+		for (int j = 0; j < 9; j++){
+		    puzzle[i][j] = ;
+		    System.out.println(puzzle[i][j]);
+		}
+	    }
+	} catch (FileNotFoundException e) {
+	    System.out.println("File not found");
+	}
+    }
+
+    //public String 
+
+    public static void main(String[] args){
+	Sudoku test = new Sudoku("puzzles.txt");
+    }
 }
