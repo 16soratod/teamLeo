4ef9343c3ab46a7fed41021a6c6b21ffc7ba80fe
16soratod <sdoken@stuy.edu>
2016-05-17 12:29:26 -0400

    updated Sudoku.java. Started solver

--- Sudoku.java
+++ Sudoku.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class Sudoku{
     private int[][] puzzle;
+    private int temp;
 
     public Sudoku(String filename){
 	try {
@@ -14,7 +15,7 @@ public class Sudoku{
 	    }
 	    for (int i = 0; i < 9; i++){
 		for (int j = 0; j < 9; j++){
-		    puzzle[i][j] = s.charAt(i*9+j) - '0';
+		    puzzle[i][j] = s.charAt(i*9+j)  - '0';
 		}
 	    }
 	} catch (FileNotFoundException e) {
@@ -31,6 +32,48 @@ public class Sudoku{
 	    retSt += "\n";
 	}
 	return retSt;
+    }
+
+    public int[][] solve(){
+    	return solveH(0,0);
+    }
+
+    public boolean solveH(int row, int col){
+    	if(col >= puzzle.length){
+    		return true;
+    	}
+    	if(puzzle[row][col] != 0){
+    		if(col < 8){
+    			solveH(row,col+1);
+    		}else{
+    			solveH(row+1,col);
+    		}
+    	}else{
+    		temp = ( int ) (Math.random(10) + 1);
+
+    	}
+    	if(checkBox(row,col) && checkHorizontal(row) && checkVertical()){
+    		if(col < 8){
+    			solveH(row,col+1);
+    		}else{
+    			solveH(row+1,col);
+    		}
+    	}
+
+    }
+
+    public boolean checkBox(int row, int col, int v){
+
+    }
+
+    public boolean checkHorizontal(int row, int v){
+    	for(int i = 0; i < 9; i++){
+    		if(puzzle[row][])
+    	}
+    }
+
+    public boolean checkVertical(int row, int v){
+
     }
 
     public static void main(String[] args){
