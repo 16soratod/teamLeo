916b0c52c9de0979c32179a8dba828d9ebfd8007
16soratod <sdoken@stuy.edu>
2016-05-18 00:55:50 -0400

    updated solver code

--- Sudoku.java
+++ Sudoku.java
@@ -34,13 +34,13 @@ public class Sudoku{
 	return retSt;
     }
 
-    public int[][] solve(){
+    public boolean solve(){
     	return solveH(0,0);
     }
 
     public boolean solveH(int row, int col){
     	if(row >= 8 && col >= 8){
-	    return true;
+	    	return true;
     	}
     	if(puzzle[row][col] != 0){
 	    if(col < 7){
@@ -66,28 +66,137 @@ public class Sudoku{
     }
 
     private int checkQuadrant(int row, int col){
-	if (row < 3 && col < 3)
+	if (row < 3 && col < 3){
 	    return 1;
-	if (row < 3 && col < 6)
+	}
+	if (row < 3 && col < 6){
 	    return 2;
-	if (row < 3 && col < 9)
+	}
+	if (row < 3 && col < 9){
 	    return 3;
-	if (row < 6 && col < 3)
+	}
+	if (row < 6 && col < 3){
 	    return 4;
-	if (row < 6 && col < 6)
+	}
+	if (row < 6 && col < 6){
 	    return 5;
-	if (row < 6 && col < 9)
+	}
+	if (row < 6 && col < 9){
 	    return 6;
-	if (row < 9 && col < 3)
+	}
+	if (row < 9 && col < 3){
 	    return 7;
-	if (row < 9 && col < 6)
+	}
+	if (row < 9 && col < 6){
 	    return 8;
-	if (row < 9 && col < 9)
+	}
+	if (row < 9 && col < 9){
 	    return 9;
+	}
+	return -1;
     }
 
     public boolean checkBox(int row, int col, int v){
-	return false;
+		if(checkQuadrant(row,col) == 1){
+			for(int i = 0; i < 3; i++){
+				for(int j = 0; j < 3; j++){
+					if(puzzle[i][j] == v){
+						return false;
+					}
+				}
+			}
+		}else{
+			return true;
+		}
+		if(checkQuadrant(row,col) == 2){
+			for(int i = 0; i < 3; i++){
+				for(int j = 3; j < 6; j++){
+					if(puzzle[i][j] == v){
+						return false;
+					}
+				}
+			}
+		}else{
+			return true;
+		}
+		if(checkQuadrant(row,col) == 3){
+			for(int i = 0; i < 3; i++){
+				for(int j = 6; j < 9; j++){
+					if(puzzle[i][j] == v){
+						return false;
+					}
+				}
+			}
+		}else{
+			return true;
+		}
+		if(checkQuadrant(row,col) == 4){
+			for(int i = 3; i < 6; i++){
+				for(int j = 0; j < 3; j++){
+					if(puzzle[i][j] == v){
+						return false;
+					}
+				}
+			}
+		}else{
+			return true;
+		}
+		if(checkQuadrant(row,col) == 5){
+			for(int i = 3; i < 6; i++){
+				for(int j = 3; j < 6; j++){
+					if(puzzle[i][j] == v){
+						return false;
+					}
+				}
+			}
+		}else{
+			return true;
+		}
+		if(checkQuadrant(row,col) == 6){
+			for(int i = 3; i < 6; i++){
+				for(int j = 6; j < 9; j++){
+					if(puzzle[i][j] == v){
+						return false;
+					}
+				}
+			}
+		}else{
+			return true;
+		}
+		if(checkQuadrant(row,col) == 7){
+			for(int i = 6; i < 9; i++){
+				for(int j = 0; j < 3; j++){
+					if(puzzle[i][j] == v){
+						return false;
+					}
+				}
+			}
+		}else{
+			return true;
+		}
+		if(checkQuadrant(row,col) == 8){
+			for(int i = 6; i < 9; i++){
+				for(int j = 3; j < 6; j++){
+					if(puzzle[i][j] == v){
+						return false;
+					}
+				}
+			}
+		}else{
+			return true;
+		}
+		if(checkQuadrant(row,col) == 9){
+			for(int i = 6; i < 9; i++){
+				for(int j = 6; j < 9; j++){
+					if(puzzle[i][j] == v){
+						return false;
+					}
+				}
+			}
+		}else{
+			return true;
+		}
+		return false;
     }
 
     public boolean checkHorizontal(int row, int v){
@@ -108,6 +217,11 @@ public class Sudoku{
 
     public static void main(String[] args){
 	Sudoku test = new Sudoku("puzzles.txt");
+	System.out.println(test.toString());
+	System.out.println(test.checkBox(0,1,9));
+	System.out.println(test.checkHorizontal(1,4));
+	System.out.println(test.checkVertical(3,7));
+	test.solve();
 	System.out.println(test.toString());
     }
 }
