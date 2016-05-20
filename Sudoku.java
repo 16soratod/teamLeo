9e296a89dc8ffa34301867af7d33139ae3349084
Elina Hvirtsman <elina.hvirtsman@gmail.com>
2016-05-19 23:25:34 -0400

    Fixed solver

--- Sudoku.java
+++ Sudoku.java
@@ -39,172 +39,41 @@ public class Sudoku{
     }
 
     public boolean solveH(int row, int col){
-    	if(row >= 8 || col >= 8){
+    	if(row == 9){
 	    	return true;
     	}
     	if(puzzle[row][col] != 0){
-	    if(col < 7){
-		solveH(row,col+1);
-	    }else{
-		solveH(row+1,0);
+	    if(col <= 7 && solveH(row,col+1)){
+		return true;
+	    }else if (col > 7 && solveH(row+1,0)){
+		return true;
 	    }
     	}else{
-	    temp = 0;
 	    for(int i = 1; i < 10; i++){
 		temp = i;
 		if(checkBox(row,col,temp) && checkHorizontal(row,temp) 
 		   && checkVertical(col,temp)){
 		    puzzle[row][col] = temp;
-		    if(col < 7){
-			solveH(row,col+1);
-		    }else{
-			solveH(row+1,0);
+		    if(col <= 7 && solveH(row,col+1)){
+			return true;
+		    }else if (col > 7 && solveH(row+1,0)){
+			return true;
 		    }
 		}
 	    }
-	    if(row == 0){
-		return false;
-	    }else if(col == 0){
-		solveH(row-1, 9);
-	    }else{
-		solveH(row, col-1);
-	    }
+	    puzzle[row][col] = 0;
 	}
 	return false;
     }
 
-    private int checkQuadrant(int row, int col){
-	if (row < 3 && col < 3){
-	    return 1;
-	}
-	if (row < 3 && col < 6){
-	    return 2;
-	}
-	if (row < 3 && col < 9){
-	    return 3;
-	}
-	if (row < 6 && col < 3){
-	    return 4;
-	}
-	if (row < 6 && col < 6){
-	    return 5;
-	}
-	if (row < 6 && col < 9){
-	    return 6;
-	}
-	if (row < 9 && col < 3){
-	    return 7;
-	}
-	if (row < 9 && col < 6){
-	    return 8;
-	}
-	if (row < 9 && col < 9){
-	    return 9;
-	}
-	return -1;
-    }
-
     public boolean checkBox(int row, int col, int v){
-		if(checkQuadrant(row,col) == 1){
-			for(int i = 0; i < 3; i++){
-				for(int j = 0; j < 3; j++){
-					if(puzzle[i][j] == v){
-						return false;
-					}
-				}
-			}
-		}else{
-			return true;
-		}
-		if(checkQuadrant(row,col) == 2){
-			for(int i = 0; i < 3; i++){
-				for(int j = 3; j < 6; j++){
-					if(puzzle[i][j] == v){
-						return false;
-					}
-				}
-			}
-		}else{
-			return true;
-		}
-		if(checkQuadrant(row,col) == 3){
-			for(int i = 0; i < 3; i++){
-				for(int j = 6; j < 9; j++){
-					if(puzzle[i][j] == v){
-						return false;
-					}
-				}
-			}
-		}else{
-			return true;
-		}
-		if(checkQuadrant(row,col) == 4){
-			for(int i = 3; i < 6; i++){
-				for(int j = 0; j < 3; j++){
-					if(puzzle[i][j] == v){
-						return false;
-					}
-				}
-			}
-		}else{
-			return true;
-		}
-		if(checkQuadrant(row,col) == 5){
-			for(int i = 3; i < 6; i++){
-				for(int j = 3; j < 6; j++){
-					if(puzzle[i][j] == v){
-						return false;
-					}
-				}
-			}
-		}else{
-			return true;
-		}
-		if(checkQuadrant(row,col) == 6){
-			for(int i = 3; i < 6; i++){
-				for(int j = 6; j < 9; j++){
-					if(puzzle[i][j] == v){
-						return false;
-					}
-				}
-			}
-		}else{
-			return true;
-		}
-		if(checkQuadrant(row,col) == 7){
-			for(int i = 6; i < 9; i++){
-				for(int j = 0; j < 3; j++){
-					if(puzzle[i][j] == v){
-						return false;
-					}
-				}
-			}
-		}else{
-			return true;
-		}
-		if(checkQuadrant(row,col) == 8){
-			for(int i = 6; i < 9; i++){
-				for(int j = 3; j < 6; j++){
-					if(puzzle[i][j] == v){
-						return false;
-					}
-				}
-			}
-		}else{
-			return true;
-		}
-		if(checkQuadrant(row,col) == 9){
-			for(int i = 6; i < 9; i++){
-				for(int j = 6; j < 9; j++){
-					if(puzzle[i][j] == v){
-						return false;
-					}
-				}
-			}
-		}else{
-			return true;
-		}
-		return false;
+	int r = row / 3 * 3;
+	int c = col / 3 * 3;
+	for (int i = r; i < r + 3; i++)
+	    for (int j = c; j < c + 3; j++)
+		if (puzzle[i][j] == v)
+		    return false;
+	return true;
     }
 
     public boolean checkHorizontal(int row, int v){
